8dcf892d69cfccb90a80a807bca2fc878c34e51b
Sanket Bagad <alencolins@gmail.com>
2021-10-16 18:24:23 +0530

    About Section - Left side

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './Components/About/About';
 import Intro from './Components/Intro/Intro';
 
 function App() {
   return (
     <div>
    <Intro />
+   <About />
     </div>
   );
 }
--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import "./About.css"
+import Ab from "../../img/About.jpg"
 
 const About = () => {
     return (
         <div className="a">
-            
+           <div className="a-left">
+               <div className="a-card bg"></div>
+               <div className="a-card">
+                   <img className="a-img" src={Ab} alt="About Sanket Bagad" />
+               </div>
+           </div>
+
+           <div className="a-right"></div> 
         </div>
     )
 }
