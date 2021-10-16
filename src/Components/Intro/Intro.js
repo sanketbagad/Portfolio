b2cf100db21db95d412b8274430d83bc78eadc54
Sanket Bagad <alencolins@gmail.com>
2021-10-16 16:05:02 +0530

    Structure for the left side of the intro sec ready

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -1,10 +1,49 @@
 import React from 'react'
+import "./intro.css"
 
 const Intro = () => {
     return (
         <div className='i'>
             <div className="i-left">
-                
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">
+                        Hello, My Name is
+                    </h2>
+                     <h1 className="i-name">Sanket Bagad</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">
+                                Full Stack Developer
+                            </div>
+                            <div className="i-title-item">
+                               React JS Developer 
+                            </div>
+                            <div className="i-title-item">
+                                Node JS Developer
+                            </div>
+                            <div className="i-title-item">
+                                Mongo DB
+                            </div>
+                            <div className="i-title-item">
+                                React Native
+                            </div>
+                            <div className="i-title-item">
+                                Competive Programming
+                            </div>
+                           
+                        </div>
+                    </div>
+                    <div className="i-desc">
+                        I design and develop cool UI's, currently for myself but would love to work for you.
+                        I use MERN stack to build full-stack applications and can also deploy them for you, as you can see my work/Projects below.
+                        If you need Something just remember <span>I will be There for you.</span>
+                    </div>
+                </div>
+            
+            
+            </div>
+            <div className="i-right">
+
             </div>
             
         </div>
