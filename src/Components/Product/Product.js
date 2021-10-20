ec241c8e620448e9a578b86124ae693a4284f03a
Sanket Bagad <alencolins@gmail.com>
2021-10-20 21:40:02 +0530

    strucuture of section

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Product = () => {
     return (
         <div className="p">
-           
+           <div className="p-browser">
+              <div className="p-circle"></div>
+              <div className="p-circle"></div>
+              <div className="p-circle"></div> 
+           </div>
         </div>
     )
 }
