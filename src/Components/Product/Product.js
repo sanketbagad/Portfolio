3fdbced3774d4e5ff6569eb58a63dda32de21539
Sanket Bagad <alencolins@gmail.com>
2021-10-20 21:35:16 +0530

    starting on individual project cards

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Product = () => {
     return (
-        <div>
-           {console.log('Product')} 
+        <div className="p">
+           
         </div>
     )
 }
