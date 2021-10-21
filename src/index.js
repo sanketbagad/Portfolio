5a4e9e0953b93d67272bc7fa1fd0335ef2afc35f
Sanket Bagad <alencolins@gmail.com>
2021-10-21 23:12:05 +0530

    Context Done

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from './context';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
