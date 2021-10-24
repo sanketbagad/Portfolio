98c25eaa5a7dbe54dbd136640682a72559b99124
Sanket Bagad <alencolins@gmail.com>
2021-10-24 20:20:15 +0530

    Adding whatsapp component

---
+++ src/Components/Whatsapp/Whatsapp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./whatsapp.css"
+
+const Whatsapp = () => {
+    return (
+        <a
+        href="https://wa.me/2348100000000"
+        class="whatsapp_float"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i class="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
+
+    )
+}
+
+export default Whatsapp
